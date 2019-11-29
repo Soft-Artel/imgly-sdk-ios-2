dffb87f48a6e7f3a2d484a0a8d6779838531d427
murat <89196242960@mail.ru>
2019-11-29 18:50:10 +0300

    version 2.1

---
+++ imglyKit/Frontend/Camera/ViewControllerrr.swift
@@ -0,0 +1,30 @@
+//
+//  ViewControllerrr.swift
+//  imglyKit iOS
+//
+//  Created by Мурат Камалов on 29.11.2019.
+//  Copyright © 2019 9elements GmbH. All rights reserved.
+//
+
+import UIKit
+
+class ViewControllerrr: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
