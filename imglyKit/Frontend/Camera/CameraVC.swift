9c1289abb91319edef79afd0becb5dc5c879a124
Murat <89196242960@mail.ru>
2020-06-04 13:08:13 +0300

    version 2.2.11

--- imglyKit/Frontend/Camera/CameraVC.swift
+++ imglyKit/Frontend/Camera/CameraVC.swift
@@ -602,15 +602,15 @@ open class IMGLYCameraViewController: UIViewController {
     }
     
     fileprivate func saveMovieWithMovieURLToAssets(_ movieURL: URL) {
-        guard PhotoEditor.saveToAlbum else{
-            do {
-                let data = try Data(contentsOf: movieURL)
-                self.delegateEditor?.saveVideo(with: data)
-                try FileManager.default.removeItem(at: movieURL)
-            } catch _ {
-            }
-            return
-        }
+//        guard PhotoEditor.saveToAlbum else{
+//            do {
+//                let data = try Data(contentsOf: movieURL)
+//                self.delegateEditor?.saveVideo(with: data)
+//                try FileManager.default.removeItem(at: movieURL)
+//            } catch _ {
+//            }
+//            return
+//        }
         PHPhotoLibrary.shared().performChanges({
             PHAssetChangeRequest.creationRequestForAssetFromVideo(atFileURL: movieURL)
             }) { success, error in
